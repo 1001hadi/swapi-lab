62eb7e1b32a4d4570f71d5f4144adbadc4c5e7f4
hadi.jaf <hadi.jaf.1001@gmail.com>
2025-05-12 15:33:12 -0600

    data fetched and display

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,37 @@
-import { useState } from 'react'
-
-import './App.css'
+import { useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
-  
+  const [starShips, setStarShips] = useState(null);
+
+  const baseUrl = "https://www.swapi.tech/api/starships";
+
+  async function handleFetch() {
+    try {
+      const res = await axios.get(baseUrl);
+      let data = res.data.results;
+      setStarShips(data);
+    } catch (err) {
+      console.error(err);
+    }
+  }
 
   return (
     <>
-     
+      <button onClick={handleFetch}>Get StarShips</button>
+      {starShips && (
+        <div>
+          <h2>StarShips:</h2>
+          <ul>
+            {starShips.map((starship) => (
+              <li key={starship.name}>{starship.name}</li>
+            ))}
+          </ul>
+        </div>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
